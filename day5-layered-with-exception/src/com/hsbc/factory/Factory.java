5f7de01d9e94d2b71e13426531cdb455bbb9e7b4
KUMARI PRIYANKA <negipriyanka321@gmail.com>
2021-10-11 23:10:31 +0530

    Day 5

---
+++ day5-layered-with-exception/src/com/hsbc/factory/Factory.java
@@ -0,0 +1,17 @@
+package com.hsbc.factory;
+
+import com.hsbc.dao.EmployeeDao;
+import com.hsbc.dao.EmployeeDaoArrayImpl;
+import com.hsbc.service.EmployeeService;
+import com.hsbc.service.EmployeeServiceImpl;
+
+public class Factory {
+	
+	public EmployeeDao getDaoInstance(){
+		return new EmployeeDaoArrayImpl();
+	}
+	public EmployeeService getServiceInstance() {
+		return new EmployeeServiceImpl();
+	}
+
+}
